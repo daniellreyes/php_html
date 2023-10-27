c1fe54dc2e8f1568d38fc0154df9d4b1e326cd58
daniellreyes <daniell.reyes26@gmail.com>
2023-10-26 20:37:47 -0600

    introduction for php with html

---
+++ imprime-texto.php
@@ -0,0 +1,21 @@
+<?php
+    $nombre = "Daniel";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "<b>Jelou maifrens desde php con html</b>"; ?>    
+
+    <?= "<i>Imprimiendo de otra forma gg</i>";?>
+    <?= "<br>yo soy $nombre , mucho gusto </br>";?>
+
+</body>
+</html>
